29244c6a2ebba75d1e2bc728ff9ecd573f17bc4a
Tyson Cashaw <tysoncashaw@Tysons-MBP.lan>
2023-08-21 11:07:59 -0400

    made changes to code

--- Paycheck.py
+++ Paycheck.py
@@ -2,4 +2,15 @@ print("Hello Ty")
 amount = input("How much was your paycheck? ")
 print("Your paycheck was: $",amount)
 tax = float(amount) * .20
+rent = float(amount) * .10
+car = float(amount) * .10
+
+
+#TODO print out how much for each 
 print("You need to save: $"+ "{:0,.2f}".format(float(tax)) + " for taxes." )
+print("You need to save: $"+ "{:0,.2f}".format(float(rent)) + " for your rent." )
+print("You need to save: $"+ "{:0,.2f}".format(float(car)) + " for your car." )
+netamount = float(amount) - (tax + rent + car)
+
+#TODO print out how much is left for you to spend
+print("This is how much left: $","{:0,.2f}".format(float(netamount)))
