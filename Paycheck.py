47b18d44a3e01b2a1032d7519c913fef20efc4a0
Tyson Cashaw <tysoncashaw@Tysons-MBP.lan>
2023-08-21 11:10:40 -0400

    removed todo

--- Paycheck.py
+++ Paycheck.py
@@ -6,11 +6,11 @@ rent = float(amount) * .10
 car = float(amount) * .10
 
 
-#TODO print out how much for each 
+
 print("You need to save: $"+ "{:0,.2f}".format(float(tax)) + " for taxes." )
 print("You need to save: $"+ "{:0,.2f}".format(float(rent)) + " for your rent." )
 print("You need to save: $"+ "{:0,.2f}".format(float(car)) + " for your car." )
 netamount = float(amount) - (tax + rent + car)
 
-#TODO print out how much is left for you to spend
+
 print("This is how much left: $","{:0,.2f}".format(float(netamount)))
